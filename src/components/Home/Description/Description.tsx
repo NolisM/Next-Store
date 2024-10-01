435fdd69d8c87c6727dce3d5c309af1502b838c1
NolisM <nolism51@yahoo.com.ar>
2024-10-01 08:53:50 -0300

    imagenes estaticas

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,9 +1,14 @@
+import styles from './Description.module.sass'
+
 export const Description=()=>{
     return(
-        <section>
-            <p>
-                descriciones
-            </p>
+        <section className={styles.Description}>
+            <img src="/images/description.jpg" alt="production marketplace" />
+            <div className={styles.Description__text}>
+                <h2>Description</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit obcaecati odit enim autem quo molestias harum facilis rem, consectetur aut quisquam, beatae alias atque, vitae id. Nihil rem impedit eaque!</p>
+
+            </div>
         </section>
     )
 }
